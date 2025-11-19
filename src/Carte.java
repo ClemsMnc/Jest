d017b5fefc454c7457047e50c53930ebd4df12dd
Magdalena <magdalena.khiat@utt.fr>
2025-11-19 17:27:11 +0100

    modification de caracter

--- src/Carte.java
+++ src/Carte.java
@@ -1,11 +1,11 @@
 public class Carte {
-    char caracter;
+    char caractere;
     char couleur;
     boolean estJoker;
     String codeTrophee;
 
     public Carte(char caracter, char couleur, boolean estJoker, String codeTrophee) {
-        this.caracter = caracter;
+        this.caractere = caracter;
         this.couleur = couleur;
         this.estJoker = estJoker;
         this.codeTrophee = codeTrophee;
